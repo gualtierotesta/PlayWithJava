d5fc766422c8b4f6fc37f84a817749ee0452bcbd
Gualtiero Testa <gualtiero.testa@factor_y.com>
2018-10-02 09:29:38 +0200

    New kata

---
+++ src/main/java/it/gualtierotesta/playwithjava/kata/AllInclusive.java
@@ -0,0 +1,21 @@
+package it.gualtierotesta.playwithjava.kata;
+
+import java.util.List;
+
+/**
+ * Input:
+ *
+ *     a string strng
+ *     an array of strings arr
+ *
+ * Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+ *
+ *     a boolean true if all rotations of strng are included in arr (C returns 1)
+ *     false otherwise (C returns 0)
+ */
+public class AllInclusive {
+
+    public static boolean containAllRots(String strng, List<String> arr) {
+        return false;
+    }
+}
