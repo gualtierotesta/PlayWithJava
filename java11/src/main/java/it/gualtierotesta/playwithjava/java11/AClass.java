8be30862ee44edc240f732b6244711fb8143eee5
Gualtiero Testa <gualtiero.testa@gmail.com>
2018-11-26 19:46:41 +0100

    Revised repo structure with two separate projects

---
+++ java11/src/main/java/it/gualtierotesta/playwithjava/java11/AClass.java
@@ -0,0 +1,8 @@
+package it.gualtierotesta.playwithjava.java11;
+
+public class AClass {
+
+    public static String aMethod() {
+        return "aResult";
+    }
+}
---
+++ java11/src/main/java/it/gualtierotesta/playwithjava/java11/Main.java
@@ -0,0 +1,9 @@
+package it.gualtierotesta.playwithjava.java11;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var msg = "Hello Java 11";
+        System.out.println(msg);
+    }
+}
