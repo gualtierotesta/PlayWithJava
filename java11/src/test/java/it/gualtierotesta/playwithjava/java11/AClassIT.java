e46aff3790350ecd055c60be12317458feffbd6f
Gualtiero Testa <gualtiero.testa@gmail.com>
2018-12-15 16:38:39 +0100

    General revision and updates

--- java11/src/test/java/it/gualtierotesta/playwithjava/java11/AClassIT.java
+++ java11/src/test/java/it/gualtierotesta/playwithjava/java11/AClassIT.java
@@ -4,11 +4,11 @@ package it.gualtierotesta.playwithjava.java11;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class AClassTest {
+public class AClassIT {
 
     @Test
     public void testAMethod() {
-        System.out.println("XXX Unit test");
-        Assertions.assertNotNull(AClass.aMethod());
+        System.out.println("XXX Integration test");
+        Assertions.assertNotNull(AClass.anotherMethod());
     }
 }
