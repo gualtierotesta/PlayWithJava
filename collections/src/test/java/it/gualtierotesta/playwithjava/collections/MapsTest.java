5b182a441a67152d5c30977c013185e487926518
Gualtiero Testa <gualtierotesta@protonmail.com>
2021-09-26 17:08:03 +0200

    Project on collections

---
+++ collections/src/test/java/it/gualtierotesta/playwithjava/collections/MapsTest.java
@@ -0,0 +1,36 @@
+package it.gualtierotesta.playwithjava.collections;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MapsTest {
+
+    private static final Integer KEY1 = Integer.valueOf(1);
+    private static final Integer KEY2 = Integer.valueOf(2);
+    private static final Integer KEY_WITHOUT_VALUE = Integer.valueOf(0);
+    private static final Integer KEY_NOT_EXISTING = Integer.valueOf(99);
+
+    private Map<Integer, String> map;
+
+    @BeforeEach
+    void beforeEach() {
+
+        map = new HashMap<>();
+
+        map = new HashMap<>();
+        map.put(KEY1, "uno");
+        map.put(KEY2, "due");
+        map.put(KEY_WITHOUT_VALUE, null);
+    }
+
+    @Test
+    void check_setup () {
+        assertThat(map.size()).isEqualTo(3);
+    }
+}
