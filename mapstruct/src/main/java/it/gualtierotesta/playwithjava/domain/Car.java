fcd521b1a2ed5ec816b40cb7a6fcd883bb28de86
Gualtiero Testa <gualtierotesta@protonmail.com>
2021-12-10 08:04:53 +0100

    [mapstruct] Additional mapping

--- mapstruct/src/main/java/it/gualtierotesta/playwithjava/domain/Car.java
+++ mapstruct/src/main/java/it/gualtierotesta/playwithjava/domain/Car.java
@@ -1,12 +1,15 @@
 package it.gualtierotesta.playwithjava.domain;
 
+import java.time.LocalDate;
+
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
 public class Car {
-	private CarMaker maker;
-	private int numberOfSeats;
-	private CarType type;
+    private CarMaker maker;
+    private int numberOfSeats;
+    private CarType type;
+    private LocalDate mftDate;
 }
