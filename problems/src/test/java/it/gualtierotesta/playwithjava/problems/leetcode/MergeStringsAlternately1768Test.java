9ba7554daf7b083417b9ba88555f83f62da2fe0e
Gualtiero Testa <gualtierotesta@protonmail.com>
2024-11-06 08:57:32 +0100

    Add new problems

---
+++ problems/src/test/java/it/gualtierotesta/playwithjava/problems/leetcode/MergeStringsAlternately1768Test.java
@@ -0,0 +1,24 @@
+package it.gualtierotesta.playwithjava.problems.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+class MergeStringsAlternately1768Test {
+
+    @Test
+    void test01() {
+        String result = mergeAlternately("cdf", "a");
+        System.out.println("result = " + result);
+    }
+
+    private static String mergeAlternately(String word1, String word2) {
+        int length = Math.min(word1.length(), word2.length());
+        String suffix = word1.substring(length) + word2.substring(length);
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(word1.charAt(i));
+            builder.append(word2.charAt(i));
+        }
+        builder.append(suffix);
+        return builder.toString();
+    }
+}
